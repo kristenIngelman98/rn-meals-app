0f14901ac587bce440b03d22915d921fe88916a8
Kristen <55934200+kristenIngelman98@users.noreply.github.com>
2021-02-01 21:17:04 -0800

    added stack navigation to main 3 screens

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-// import { StatusBar } from 'expo-status-bar';
-// import React, { useState } from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-// import * as Font from 'expo-font';
-// import { AppLoading } from 'expo-app-loading';
+import React, { useState } from 'react';
+import { Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo-app-loading';
+import MealsNavigator from './navigation/MealsNavigator';
 
 // const fetchFonts = () => {
 //   return Font.loadAsync({
@@ -11,56 +11,18 @@
 //   });
 // };
 
-// export default function App() {
-//   const [fontLoaded, setFontLoaded] = useState(false);
-
-//   // this makes sure app stays on splash screen until all fonts are loaded
-//   if (!fontLoaded) {
-//     return (
-//     <AppLoading
-//       startAsync={fetchFonts}
-//       onFinish={() => setFontLoaded(true)}
-//       onError={(err) => console.log(err)}
-//   />
-//     );
-//   }
- 
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//     </View>
-//   );
-// }
-
-
-import React, { useState } from 'react';
-import { Text, View } from 'react-native';
-import * as Font from 'expo-font';
-import { AppLoading } from 'expo-app-loading';
-
-const fetchFonts = () => {
-  return Font.loadAsync({
-    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
-    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
-  });
-};
-
 export default function App() {
   const [fontLoaded, setFontLoaded] = useState(false);
 
-  if (!fontLoaded) {
-    return (
-      <AppLoading
-        startAsync={fetchFonts}
-        onFinish={() => setFontLoaded(true)}
-        onError={(err) => console.log(err)}
-      />
-    );
-  }
+  // if (!fontLoaded) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchFonts}
+  //       onFinish={() => setFontLoaded(true)}
+  //       onError={(err) => console.log(err)}
+  //     />
+  //   );
+  // }
 
-  return (
-    <View>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <MealsNavigator />;
 }
